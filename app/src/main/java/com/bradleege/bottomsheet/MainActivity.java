441c868284643f581a3184850246bacea861de2d
Brad Leege <bleege@gmail.com>
2016-04-02 14:58:23 -0500

    Enabling opening of Bottom Sheet via Button 1

--- app/src/main/java/com/bradleege/bottomsheet/MainActivity.java
+++ app/src/main/java/com/bradleege/bottomsheet/MainActivity.java
@@ -1,13 +1,44 @@
 package com.bradleege.bottomsheet;
 
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private BottomSheetBehavior bottomSheetBehavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View bottomSheet = findViewById(R.id.bottom_sheet);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        Button button2 = (Button) findViewById(R.id.button_2);
+        Button button3 = (Button) findViewById(R.id.button_3);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        switch( v.getId() ) {
+            case R.id.button_1: {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                break;
+            }
+        }
     }
 }
